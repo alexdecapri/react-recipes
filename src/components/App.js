880e649fd74ede7feb7a154f9b8be59127f69195
Alex DeCapri <alexdecapri@gmail.com>
2016-08-11 15:50:47 -0600

    Updates to search_bar and axios request, CORS error

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../styles/App.css';
+
+const API_KEY = '2bf85f602ad897ed67b5267d105df900';
+const ROOT_URL = 'http://food2fork.com/api/search';
+
+function fetchRecipes() {
+  // const url = `${ROOT_URL}&q=${food}`;
+  const url = `${ROOT_URL}&key=${API_KEY}&q=chicken`;
+  return axios.get(url)
+    .then(function(response) {
+      console.log(response);
+    })
+
+}
+
+fetchRecipes();
 
 class App extends Component {
   render() {
